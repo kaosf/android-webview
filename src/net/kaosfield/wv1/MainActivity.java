6b75089678cdb35bb79b2b896721867a89a118c4
ka <ka.kaosf@gmail.com>
2013-05-01 17:21:04 +0900

    use WebView

--- src/net/kaosfield/wv1/MainActivity.java
+++ src/net/kaosfield/wv1/MainActivity.java
@@ -3,6 +3,7 @@ package net.kaosfield.wv1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		WebView webView = (WebView) findViewById(R.id.webview);
+		webView.loadUrl("http://google.com");
 	}
 
 	@Override

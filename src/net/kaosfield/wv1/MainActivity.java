f14c06d7f8d9c6274f573a6ccc84fea0c15f437c
ka <ka.kaosf@gmail.com>
2013-05-01 17:49:11 +0900

    enable to go back with the back button

--- src/net/kaosfield/wv1/MainActivity.java
+++ src/net/kaosfield/wv1/MainActivity.java
@@ -2,18 +2,21 @@ package net.kaosfield.wv1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
+	private WebView webView = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		WebView webView = (WebView) findViewById(R.id.webview);
+		webView = (WebView) findViewById(R.id.webview);
 		webView.setWebViewClient(new MyWebViewClient());
 		webView.loadUrl("http://google.com");
 	}
@@ -23,6 +26,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Check if the key event was the Back button and if there's history
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
+			return true;
+		}
+		// If it wasn't the Back key or there's no web page history, bubble up to the default
+		// system behavior (probably exit the activity)
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
